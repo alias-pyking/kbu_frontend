087c6e2e0f29f06fec9626ca49b96c72e057bdce
shubhamsks <shubhamkumar7051@gmail.com>
2020-12-06 10:18:46 +0530

    fixed cardtoo l

--- src/components/CardTool/CardTool.js
+++ src/components/CardTool/CardTool.js
@@ -22,15 +22,10 @@ function cardTool(props) {
             <Card.Content>
                 <Card.Header>{props.name}</Card.Header>
                 <Card.Meta>
-                    <span className='date'>Added {timeAgo.format(new Date(props.timestamp))}</span>
-                    <Rating icon='star' defaultRating={3.5} maxRating={5} disabled/>
-                    <Card.Header className='theme-color'>{props.name}</Card.Header>
                     <Card.Meta>
                         <span className='date'>Added {getTimeAgo(props.timestamp)}</span>
+                        <Rating icon='star' defaultRating={3.5} maxRating={5} disabled/>
                     </Card.Meta>
-                    <Card.Description>
-                        {description}
-                    </Card.Description>
                 </Card.Meta>
                 <Card.Description>
                     {description}
