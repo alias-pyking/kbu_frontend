7264688005f38f7aa247276dcd9d955006dd4401
Pushpendra kumar <pushpendra466@gmail.com>
2020-12-06 19:53:30 +0530

    change in nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,6 +14,7 @@ function NavBar (){
                 <NavBarItem to='/tools' name='Tools'  />
                 <NavBarItem to='/about' name='About'  />
                 <NavBarItem to='/auth' name='Login/Register' />
+            
             </Menu>
         </Segment>
        
